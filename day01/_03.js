ec5cbfb455e0d6e82f99a8a7af8f03f4b4685b75
Rudra Makhija <140305994+RudraMakhija@users.noreply.github.com>
2024-06-28 13:48:31 +0530

    Add files via upload

---
+++ day01/_03.js
@@ -0,0 +1,12 @@
+/*
+Why do we use the word “debugger” in javascript?
+*/
+
+/*
+
+The word debugger is used to pause the execution of the script at a specific point.
+It helps in knowing the state of application at a specific point like knowing the value of variables or analysing the call stack and scope.
+
+It helps in quick debugging, placing the word debugger is faster than navigating through the debugging tools to set a breakpoint.
+
+*/
